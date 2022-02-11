27e20aa9fa4031efb811c725560c2beead9067e4
Gary Benson <gary@gbenson.net>
2022-02-11 10:12:50 +0000

    template

---
+++ warpboard.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    if "sys" not in locals():
+        import sys
+    assert sys.version_info >= (3,)
+    main()
